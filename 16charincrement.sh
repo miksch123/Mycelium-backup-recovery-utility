aac2c8df086d2c33f109ce3f367c7bd47de01913
miksch123 <44267886+miksch123@users.noreply.github.com>
2021-10-12 09:16:24 +0200

    Create 16charincrement.sh
    incrementer for 16 char password

---
+++ 16charincrement.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+for a in {A..Z}; do 
+for b in {A..Z}; do 
+for c in {A..Z}; do 
+for d in {A..Z}; do 
+for e in {A..Z}; do 
+for f in {A..Z}; do 
+for g in {A..Z}; do 
+for h in {A..Z}; do
+for i in {A..Z}; do 
+for j in {A..Z}; do 
+for k in {A..Z}; do 
+for l in {A..Z}; do
+for m in {A..Z}; do 
+for n in {A..Z}; do 
+for o in {A..Z}; do 
+for p in {A..Z}; do
+echo -n " $a$b$c$d$e$f$g$h$i$j$k$l$m$n$o$p"
+done 
+done 
+done 
+done
+done 
+done 
+done 
+done
+done 
+done 
+done 
+done
+done 
+done 
+done 
+done
+
